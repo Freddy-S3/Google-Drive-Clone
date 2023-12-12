577e1898d863dd2544e4f078427dd906875799c8
Freddy-S3 <141098248+Freddy-S3@users.noreply.github.com>
2023-12-12 16:44:43 -0500

    ID required to upload

--- database/migrations/2023_04_21_035732_create_file_shares_table.php
+++ database/migrations/2023_04_21_035732_create_file_shares_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('file_shares', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('file_id')->nullable()->constrained('files');
-            $table->foreignId('user_id')->nullable()->constrained('users');
+            $table->foreignId('file_id')->constrained('files');
+            $table->foreignId('user_id')->constrained('users');
             $table->timestamps();
         });
     }
